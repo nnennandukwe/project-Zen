81d5d7a6a8de8cbfb662400a706e9867599b54c7
Nnenna Ndukwe <nnenna.s.ndukwe@gmail.com>
2017-05-07 13:48:21 -0400

    This is a nonprofit organization site I created for encouraging meditation in public schools

---
+++ projectzen.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+		{document.getElementById("topBtn").style.display="block";
+	} else {
+		document.getElementById("topBtn").style.display="none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
